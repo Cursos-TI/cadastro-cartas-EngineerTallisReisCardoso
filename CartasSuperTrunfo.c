44b403c36a979d3eb5ff5deecc167ccce0b5db91
EngineerTallisReisCardoso <vevekogamer@gmail.com>
2026-05-04 16:49:30 +0000

    Consertei bugs e adicionei o modo cartas vs cartas comparando e decidindo o campeão!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,8 +12,8 @@
     float area1;
     long long int pib1;
     unsigned int pontos_turisticos1;
-    float densidade_populacional1;
-    float pib_per_capita1;
+    double densidade_populacional1;
+    double pib_per_capita1;
     
         
 
@@ -25,8 +25,8 @@
     float area2;
     long long int pib2;
     unsigned int pontos_turisticos2;
-    float densidade_populacional2;
-    float pib_per_capita2;
+    double densidade_populacional2;
+    double pib_per_capita2;
     
                
 
@@ -78,7 +78,7 @@
     scanf("%f", &area2);
 
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &pib2);
+    scanf("%lld", &pib2);
 
     printf("Digite quantos pontos turisticos tem nessa cidade");
     scanf("%u", &pontos_turisticos2);
@@ -99,10 +99,10 @@
     printf("Número de pontos turisticos: %u\n", pontos_turisticos1);
     
     densidade_populacional1 = populacao1 / area1;
-    printf("O a densidade populacional é: %.2f\n", densidade_populacional1);
+    printf("O a densidade populacional é: %lf\n", densidade_populacional1);
     
     pib_per_capita1 = pib1 / populacao1;
-    printf("O PIB per capita é: %.2f\n", pib_per_capita1);
+    printf("O PIB per capita é: %lf\n", pib_per_capita1);
 
 
 
@@ -117,24 +117,59 @@
 
     
     printf("--CARTA 1--\n")
-    float densidade_populacional1 = populacao1 / area1;
-    printf("O a densidade populacional da cidade %s é: %.2f\n", cidade1, densidade_populacional1);
+    double densidade_populacional1 = (double) populacao1 / area1;
+    printf("O a densidade populacional da cidade %s é: %lf\n", cidade1, densidade_populacional1);
     
-    float pib_per_capita1 = pib1 / populacao1;
-    printf("O PIB per capita da cidade %s é: %.2f\n", cidade1, pib_per_capita1);
+    double pib_per_capita1 = pib1 / populacao1;
+    printf("O PIB per capita da cidade %s é: %lf\n", cidade1, pib_per_capita1);
     
     printf("--CARTA 2--\n")
-   float densidade_populacional2 = populacao2 / area2;
-    printf("O a densidade populacional da cidade %s é: %.2f\n", cidade2, densidade_populacional2);
+    double densidade_populacional2 = (double) populacao2 / area2;
+    printf("O a densidade populacional da cidade %s é: %lf\n", cidade2, densidade_populacional2);
     
-   float pib_per_capita2 = pib2 / populacao2;
-   printf("O PIB per capita da cidade %s é: %.2f\n", cidade2, pib_per_capita2);
+    double pib_per_capita2 = pib2 / populacao2;
+   printf("O PIB per capita da cidade %s é: %lf\n", cidade2, pib_per_capita2);
 
    printf("Quem tem mais super poder é o vencedor\n");
    printf("--1 para verdadeiro, e 0 para falso");
    printf("A cidade da carta 1 %s é maior que a cidade da carta 2 %s: %d\n", cidade1, cidade2, (superPoder1 > superPoder2));
    printf("A cidade da carta 1 %s é menor que a cidade da carta 2 %s: %d\n", cidade1, cidade2, (superPoder1 < superPoder2));
     
-    return 0;
+             // COMPARANDOS A CARTA 1 VS A CARTA 2
+     
+             //comparando população
+   if(populacao1 > populacao2){
+      printf("A cidade %s venceu!!!", cidade1);
+   }else{
+      printf("A cidade %s venceu!!!", cidade2);
+   }
+            // comparando area
+   if(area1 > area2){
+      printf("A cidade %s venceu!!!", cidade1);
+   }else{
+      printf("A cidade %s venceu!!!", cidade2);
+   }
+
+          // comparando pib
+    if(pib1 > pib2){
+       printf("A cidade %s venceu!!!", cidade1);
+    }else {
+       printf("A cidade %s venceu!!!", cidade2);
+    }
+          
+          // comparando pontos turisticos
+    if(pontos_turisticos1 > pontos_turisticos2){
+       printf("A cidade %s venceu!!!", cidade1);
+    }
+       printf("A cidade %s venceu!!!", cidade2);
+
+          // comparando super poder
+    if(superPoder1 > superPoder2){
+       printf("A cidade %s venceu!!!", cidade1);
+    }else{
+       printf("A cidade %s venceu!!!", cidade2);
+    }
+
+return 0;
 
  }
